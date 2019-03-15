2243c5da2d9b03da45290586435aca7d000205c9
Leticia dos Santos <leticia_Santos250@hotmail.com>
2019-03-15 17:09:36 -0300

    50% página de comidas

---
+++ app/view/tipoComida.php
@@ -0,0 +1,143 @@
+<?php
+	include("cabecalho.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Comidas</title>
+</head>
+<body>
+<div  style="padding-top: 1%">
+	<div class="ui special cards" style="margin-left: 6.5%">
+  <div class="card" style="width: 30%">
+    <div class="blurring dimmable image">
+      <div class="ui dimmer">
+        <div class="content">
+          <div class="center">
+            <div class="ui inverted button">Comida Japonesa</div>
+          </div>
+        </div>
+      </div>
+      <img src="../../assets/img/tipo_comida/sushi.jpg">
+    </div>
+
+   </div>
+  <div class="card" style="width: 30%">
+    <div class="blurring dimmable image">
+      <div class="ui dimmer">
+        <div class="content">
+          <div class="center">
+            <div  class="ui inverted button">Fast Food</div>
+          </div>
+        </div>
+      </div>
+      <img src="../../assets/img/tipo_comida/fast-food.jpg">
+    </div>
+
+ </div>
+  <div class="card" style="width: 30%">
+    <div class="blurring dimmable image">
+      <div class="ui dimmer">
+        <div class="content">
+          <div class="center">
+            <div  class="ui inverted button">Fast Food</div>
+          </div>
+        </div>
+      </div>
+      <img src="../../assets/img/tipo_comida/fast-food.jpg">
+    </div>
+
+  </div>
+  </div>
+  <div  style="padding-top: 2%">
+  <div class="ui special cards" style="margin-left: 6.5%">
+  <div class="card" style="width: 30%">
+    <div class="blurring dimmable image">
+      <div class="ui dimmer">
+        <div class="content">
+          <div class="center">
+            <div  class="ui inverted button">Comida Italiana</div>
+          </div>
+        </div>
+      </div>
+      <img src="../../assets/img/tipo_comida/italiana.jpg">
+    </div>
+
+
+
+   </div>
+  <div class="card" style="width: 30%">
+    <div class="blurring dimmable image">
+      <div class="ui dimmer">
+        <div class="content">
+          <div class="center">
+            <div  class="ui inverted button">Comida Vegetariana</div>
+          </div>
+        </div>
+      </div>
+      <img src="../../assets/img/tipo_comida/vegetariana.jpg">
+    </div>
+
+    </div>
+  <div class="card" style="width: 30%">
+    <div class="blurring dimmable image">
+      <div class="ui dimmer">
+        <div class="content">
+          <div class="center">
+            <div  class="ui inverted button">Comida Vegetariana</div>
+          </div>
+        </div>
+      </div>
+      <img src="../../assets/img/tipo_comida/vegetariana.jpg">
+    </div>
+
+  </div>
+  
+
+
+
+
+
+
+</div><br>
+<div class="ui card">
+	<div class="content">
+		<div class="header" style="margin-left: 25%">Comentários</div>
+	</div>
+	<div class="content">
+		<div class="ui feed">
+			<div class="event">
+				<div class="label">
+					<img src="https://semantic-ui.com/images/avatar/large/jenny.jpg"/>
+				</div>
+				<div class="content">
+					<div class="date">1 day ago</div>
+					<div class="summary">You added <a>Jenny Hess</a> to your <a>coworker</a> group.</div>
+				</div>
+			</div>
+			<div class="event">
+				<div class="label">
+					<img src="https://playjoor.com/assets/avatar/elliot.jpg"/>
+				</div><div class="content">
+					<div class="date">3 days ago</div>
+					<div class="summary">Ótimos restaurantes. <a>Churrascaria Fuego</a> recomendo muito.</div>
+				</div>
+			</div>
+			<div class="event">
+				<div class="label">
+					<img src="https://semantic-ui.com/images/avatar2/large/molly.png"/>
+				</div>
+				<div class="content">
+					<div class="date">4 days ago</div>
+					<div class="summary">You added <a>Elliot Baker</a> to your <a>musicians</a> group.</div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+</body>
+</html>
+<?php include "rodape.php";?>
