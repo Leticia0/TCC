82c3550da5a31f8962eb99bf0c122614b42e6f28
Leticia dos Santos <leticia_Santos250@hotmail.com>
2019-03-22 21:09:05 -0300

    Classes
    continuei fazendo a modelagem do php na clase usuário e adicionei as classes certinho

--- app/model/usuario.php
+++ app/model/usuario.php
@@ -4,24 +4,47 @@ class Usuario
 {
     private $id;
     private $nome;
+    private $sobrenome;
     private $email;
     private $senha;
     private $dt_nasc;
 
-    public function cadastrarRestaurante{
-        //função
+    public function __construct($id, $nome, $sobrenome, $email, $senha, $dt_nasc){
+    	
+    	$this->setIdUsuario($id);
+    	$this->setNome($nome);
+    	$this->setSobrenome($sobrenome);
+    	$this->setEmail($email);
+    	$this->setSenha($senha);
+    	$this->setDataNascimento($dt_nasc);
     }
 
-    public function denunciarRestaurante{
-    	//código
+
+    public function getAlIdusuario(){
+        return $this->id;
     }
 
-	public function comentar{
-		//função
-	}
- 	
- 	public function denunciarComentario{
-	    //código
-	}
+    public function getAlIdusuario(int $IdUsuario){
+        $this->IdUsuario = $id;
+    }
 
+    public function getNome($nome){
+        return $this->nome;
+    }
+
+    public function setSobrenome($sobrenome){
+       return $this->sobrenome;
+    }
+
+    public function setEmail($email){
+       return $this->sobrenome;
+    }
+
+    public function setSenha($senha){
+       return $this->senha;
+    }
+
+    public function setDataNascimento($dt_nasc){
+       return $this->dt_nasc;
+    }
 }
